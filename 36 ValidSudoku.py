5ad6b62387329e37c685d551003e1ef8af604435
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-08-26 00:35:54 +0530

    Update 36 ValidSudoku.py

--- 36 ValidSudoku.py
+++ 36 ValidSudoku.py
@@ -1,26 +1,82 @@
-def isValidSudoku( board):
-        width = len(board[0])
-        height = len(board)
-        
-        for i in range(width + 1):
-            stringSol = ""
-            for j in range(height + 1):
-                if (bool(stringSol.find(board[i][j])) and board[i][j]!= '.'):
-                    return False
-                else:
-                    stringSol += board[i][j]
-            return True
+# source : https://leetcode.com/problems/valid-sudoku/submissions/
 
+"""
+Runtime: 116 ms, faster than 50.32% of Python3 online submissions for Valid Sudoku.
+Memory Usage: 13.6 MB, less than 99.09% of Python3 online submissions for Valid Sudoku.
+"""
+#def isValidSudoku(self, board: List[List[str]]) -> bool:
+def isValidSudoku(board):
+    #rows = len(board[0])
+    #cols = len(board)
+    # always 9
+
+    
+    return rowCheck(board) and colCheck(board) and gridCheck(board)
+
+def gridCheck(board):
+    for i in range(0,9,3):
+        for j in range(0, 9,3):
+            stringSol = ""
+            for k in range(i,i+3):
+                for l in range(j, j+3):
+                    # k l to go over a grid
+                    if (board[k][l]!= '.'):
+                        if (stringSol.find(board[k][l]) != -1 ):
+                            #print(stringSol.find(board[i][j]))
+                            #print(k,l, board[k][l])
+                            return False
+                        else:
+                            stringSol += board[k][l]
+
+    return True
+    
+
+def rowCheck(board,istart=0, jstart=0, offset=9):
+    #print(istart, jstart, offset)
+    for i in range(istart, istart + offset):
+            stringSol = ""
+            for j in range(jstart, jstart + offset):
+                if (board[i][j]!= '.'):
+                    if (stringSol.find(board[i][j]) != -1 ):
+                        #print(stringSol.find(board[i][j]))
+                        #print(i,j, board[i][j])
+                        return False
+                    else:
+                        stringSol += board[i][j]
+    return True
+
+def colCheck(board,istart=0, jstart=0, offset=9):
+    #print(istart, jstart, offset)
+    for i in range(istart, istart + offset):
+            stringSol = ""
+            for j in range(jstart, jstart + offset):
+                if (board[j][i]!= '.'):
+
+                    if (stringSol.find(board[j][i]) != -1 ):
+                        #print(stringSol.find(board[j][i]))
+                        #print(i,j, board[j][i])
+                        return False
+                    else:
+                        stringSol += board[j][i]
+                
+    return True
+        
+        
+        
+"""
+        
 Input = [
-  ["5","3",".",".","7",".",".",".","."],
-  ["6",".",".","1","9","5",".",".","."],
-  [".","9","8",".",".",".",".","6","."],
-  ["8",".",".",".","6",".",".",".","3"],
-  ["4",".",".","8",".","3",".",".","1"],
-  ["7",".",".",".","2",".",".",".","6"],
-  [".","6",".",".",".",".","2","8","."],
-  [".",".",".","4","1","9",".",".","5"],
-  [".",".",".",".","8",".",".","7","9"]
-]
+[".",".",".",".","5",".",".","1","."],
+[".","4",".","3",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","1"],
+["8",".",".",".",".",".",".","2","."],
+[".",".","2",".","7",".",".",".","."],
+[".","1","5",".",".",".",".",".","."],
+[".",".",".",".",".","2",".",".","."],
+[".","2",".","9",".",".",".",".","."],
+[".",".","4",".",".",".",".",".","."]]
+
 
 print(isValidSudoku(Input))
+
+"""
