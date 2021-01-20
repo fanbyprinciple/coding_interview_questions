5f7b951d7ccb609b0cb2d741c3514caeb8d6c5ac
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2021-01-20 16:48:40 +0530

    added exploitation code

--- security/picoctf/c_examples/soa.c
+++ security/picoctf/c_examples/soa.c
@@ -1,14 +1,19 @@
 # include <stdio.h>
 # define BUFSIZE 4
 
+//0x555555555189
 void win() {
     puts("If aI am printed, I am hacked !");
 }
 
 void vuln(){
     puts("Input a string and it will be printed:");
-    char buf[BUFSIZE];
-    gets(buf);
+    char buf[BUFSIZE]; //stack
+    gets(buf); //0x00000000000011c4
+    /* 
+     0x00000000000011c9 <+41>:    lea    -0x4(%rbp),%rax
+     0x00000000000011cd <+45>:    mov    %rax,%rdi
+    */
     puts(buf);
     fflush(stdout);
 }
